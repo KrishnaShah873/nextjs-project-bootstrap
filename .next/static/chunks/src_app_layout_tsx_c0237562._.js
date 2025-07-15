1f03c013d5f9c2f5d7f631bb6e912308be1c1758
KrishnaShah873 <kg6794160@gmail.com>
2025-07-15 20:31:34 +0545

    Add .next/static/chunks/src_app_layout_tsx_c0237562._.js 1752590794892

---
+++ .next/static/chunks/src_app_layout_tsx_c0237562._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_next_dist_6a526b48._.js",
+  "static/chunks/src_context_CartContext_tsx_8f3f6664._.js"
+],
+    source: "dynamic"
+});
